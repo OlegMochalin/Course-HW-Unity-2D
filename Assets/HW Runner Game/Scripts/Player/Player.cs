e204321f8b88d3fd1d52b9f13354690684b03178
OlegMochalin <olegmochalin@gmail.com>
2023-10-17 15:33:33 +0300

    HUB PC test 2
    here is a text

--- Assets/HW Runner Game/Scripts/Player/Player.cs
+++ Assets/HW Runner Game/Scripts/Player/Player.cs
@@ -10,7 +10,6 @@ namespace HWRunnerGame
         public event UnityAction<float> HealthChanged;
         public event UnityAction Died;
 
-
         private void Start()
         {
             HealthChanged?.Invoke(_health);
